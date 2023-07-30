33615660e066221bc7bdfbde315d852bdfd51162
Em3L <martink_lopez@yahoo.com>
2023-07-30 20:54:01 +1000

    updated

--- Lib/shapes.js
+++ Lib/shapes.js
@@ -1,33 +1,53 @@
-class Circle {
+// shapes.js
+
+class Shape {
+  constructor() {
+    this.color = 'black'; // Default shape color
+  }
+
+  setColor(color) {
+    this.color = color;
+  }
+
+  // Method to be implemented by child classes
+  getSVGString() {
+    throw new Error('getSVGString() method must be implemented by child classes.');
+  }
+}
+
+class Circle extends Shape {
   constructor(radius) {
+    super();
     this.radius = radius;
   }
 
-  getSVGString(color) {
-    return `<circle cx="150" cy="100" r="${this.radius}" fill="${color}" />`;
+  getSVGString() {
+    return `<circle cx="150" cy="100" r="${this.radius}" fill="${this.color}" />`;
   }
 }
 
-class Triangle {
+class Triangle extends Shape {
   constructor(sideLength) {
+    super();
     this.sideLength = sideLength;
   }
 
-  getSVGString(color) {
+  getSVGString() {
     const halfSide = this.sideLength / 2;
     const height = (Math.sqrt(3) / 2) * this.sideLength;
-    return `<polygon points="150,${100 - halfSide} ${150 - height},${100 + halfSide} ${150 + height},${100 + halfSide}" fill="${color}" />`;
+    return `<polygon points="150,${100 - halfSide} ${150 - height},${100 + halfSide} ${150 + height},${100 + halfSide}" fill="${this.color}" />`;
   }
 }
 
-class Square {
+class Square extends Shape {
   constructor(sideLength) {
+    super();
     this.sideLength = sideLength;
   }
 
-  getSVGString(color) {
+  getSVGString() {
     const halfSide = this.sideLength / 2;
-    return `<rect x="${150 - halfSide}" y="${100 - halfSide}" width="${this.sideLength}" height="${this.sideLength}" fill="${color}" />`;
+    return `<rect x="${150 - halfSide}" y="${100 - halfSide}" width="${this.sideLength}" height="${this.sideLength}" fill="${this.color}" />`;
   }
 }
 
--- Lib/shapes.test.js
+++ Lib/shapes.test.js
@@ -1,23 +1,31 @@
+// shapes.test.js
+
 const { Circle, Triangle, Square } = require('./shapes');
 
-test('Circle should return correct SVG string', () => {
-  const circle = new Circle(30);
-  const svgString = circle.getSVGString('red');
-  expect(svgString).toBe('<circle cx="150" cy="100" r="30" fill="red" />');
+describe('Circle', () => {
+  test('getSVGString() method returns the correct SVG string', () => {
+    const circle = new Circle(30);
+    circle.setColor('blue');
+    expect(circle.getSVGString()).toEqual('<circle cx="150" cy="100" r="30" fill="blue" />');
+  });
 });
 
-test('Triangle should return correct SVG string', () => {
-  const triangle = new Triangle(40);
-  const svgString = triangle.getSVGString('blue');
-  expect(svgString).toBe(
-    '<polygon points="150,80 110,120 190,120" fill="blue" />'
-  );
+describe('Triangle', () => {
+  test('getSVGString() method returns the correct SVG string', () => {
+    const triangle = new Triangle(40);
+    triangle.setColor('red');
+    expect(triangle.getSVGString()).toEqual(
+      '<polygon points="150,60 110,140 190,140" fill="red" />'
+    );
+  });
 });
 
-test('Square should return correct SVG string', () => {
-  const square = new Square(50);
-  const svgString = square.getSVGString('green');
-  expect(svgString).toBe(
-    '<rect x="125" y="75" width="50" height="50" fill="green" />'
-  );
+describe('Square', () => {
+  test('getSVGString() method returns the correct SVG string', () => {
+    const square = new Square(50);
+    square.setColor('green');
+    expect(square.getSVGString()).toEqual(
+      '<rect x="125" y="75" width="50" height="50" fill="green" />'
+    );
+  });
 });
